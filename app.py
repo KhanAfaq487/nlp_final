0a5459c16cc92d0f1d8b3b16e5aeb45230c8e92b
KhanAfaq487 <126563356+KhanAfaq487@users.noreply.github.com>
2024-01-08 23:54:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-# Bring in deps
-import os
 
 from langchain_community.chat_models import ChatOpenAI
 
-from apikey import apikey
 
 import streamlit as st 
 from langchain.llms import OpenAI
@@ -11,8 +8,6 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper
-
-os.environ['OPENAI_API_KEY'] = apikey
 
 # App framework
 st.title('🦜🔗 YouTube GPT Creator')
